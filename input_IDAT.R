725d473f7ed1c54bf5f9e71539f872c6001a3b7c
SergiAguilo <51328442+SergiAguilo@users.noreply.github.com>
2019-06-16 13:53:33 +0200

    Update input_IDAT.R

--- input_IDAT.R
+++ input_IDAT.R
@@ -1,3 +1,3 @@
 app <- commandArgs(trailingOnly = T)
 
-rmarkdown::render(input = "Report_IDAT.Rmd", params = list(args = app))
+rmarkdown::render(input = "Report_IDAT.Rmd", params = list(Folder = app))
